86dd442044bac2cc360cf945580c254a668854f9
TooLK5 <Pitr.pokir@mail.ru>
2019-04-12 20:31:45 +0300

    Double commit

--- JS.js
+++ JS.js
@@ -2,7 +2,9 @@ var arr = [],
 arr2 = [],
 N = 20,
 container,
-value = 99; 
+value = 99;
+
+
 
 for (let i = 0; i < N; i++) {
 	arr.push(Math.round(Math.random() * 100));
@@ -11,7 +13,7 @@ for (let i = 0; i < N; i++) {
 document.write('<br>' + arr);
 
 for (let j = 0; j < N; j++) {
-	for (i = 0; i < N; i++) {
+	for (i = 0; i < N-1; i++) {
 		if (arr[i+1] < arr[i]) {
 			container = arr[i+1];
 			arr[i+1] = arr[i];
@@ -19,6 +21,17 @@ for (let j = 0; j < N; j++) {
 		}
 	}
 }
+
+for (let i = 0; i < arr.length; i++) {
+	var MAXval = arr[0];
+	if(arr[i] > MAXval) {
+		MAXval = arr[i];
+	}
+}
+
+alert(MAXval);
+
+
 document.write('<br>' + arr);
 
 function binary(defined, value) {
